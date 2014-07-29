3256c9be71ec398864089c35b40c3fbd9c55fb80
Guillaume Claret <dev@clarus.me>
2014-07-29 16:11:45 +0200

    Module example

--- test/ex1.v
+++ test/ex1.v
@@ -3,8 +3,8 @@ Extraction Language Clojure.
 Definition twelve : nat := 2 * 5 + 2.
 Recursive Extraction twelve.
 
-(* Module M.
+Module M.
   Definition f (A : Type) (x : A) : A :=
     x.
 End M.
-Recursive Extraction M.f. *)
+Recursive Extraction M.f.
