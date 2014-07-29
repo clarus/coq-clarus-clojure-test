cb2942e63232e3bda686e764bf8677a431f0d668
Guillaume Claret <dev@clarus.me>
2014-07-29 19:30:51 +0200

    Larger example to test the extraction

--- test/ex1.v
+++ test/ex1.v
@@ -3,8 +3,22 @@ Extraction Language Clojure.
 Definition twelve : nat := 2 * 5 + 2.
 Recursive Extraction twelve.
 
+Recursive Extraction Coq.Init.Peano.plus.
+
 Module M.
   Definition f (A : Type) (x : A) : A :=
     x.
 End M.
-Recursive Extraction M.f.
+Definition zero := M.f _ 0.
+
+Recursive Extraction zero.
+
+Module N := M.
+
+Recursive Extraction N.f.
+
+Require Import Coq.Lists.List.
+Recursive Extraction list_prod.
+
+Require Import Coq.Reals.R_sqrt.
+Recursive Extraction sqrt.
