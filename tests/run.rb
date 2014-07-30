d5223e8adb0fb3a2814ba010d46c09d12f3bda5e
Guillaume Claret <dev@clarus.me>
2014-07-30 15:16:47 +0200

    Cleaner test system

--- tests/run.rb
+++ tests/run.rb
@@ -1,9 +1,8 @@
 require 'fileutils'
 
 Dir.glob("*.v") do |filename|
-  puts "#{filename}:"
   system("coqc", filename)
   FileUtils.mv("extraction.clj", "../sample-app/src/")
-  system("sed s/%%example-name%%/#{File.basename(filename, ".v")}/ ../sample-app/src/sample_app/core.clj.template > ../sample-app/src/sample_app/core.clj")
+  system("sed s/%%example-name%%/#{File.basename(filename, ".v")}/g ../sample-app/src/sample_app/core.clj.template > ../sample-app/src/sample_app/core.clj")
   system("cd ../sample-app && lein run")
 end
