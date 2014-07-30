bf56da98a45e45ca0ba4db819f8068c8a97a36a8
Guillaume Claret <dev@clarus.me>
2014-07-30 15:23:06 +0200

    Test valid

--- tests/Ex3.v
+++ tests/Ex3.v
@@ -3,6 +3,8 @@ Require Import Coq.Lists.List.
 
 Extraction Language Clojure.
 
-Definition main := list_prod.
+Definition l := List.map (fun n => n + 1) (1 :: 2 :: 3 :: nil).
+
+Definition main := Nat.eqb (List.hd 15 l) 2.
 
 Extraction "extraction" main.
